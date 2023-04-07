da58e95ec5877e1c56c53d308f73e0ff62ca8061
Flora Afroza <afzflora@gmail.com>
2023-04-07 05:57:47 -0400

    c source code file added

---
+++ aimbot/aimbot.c
@@ -0,0 +1,42 @@
+/*
+
+Have fun!
+
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <curses.h>
+
+bool *auto_mode;
+bool *is_running;
+
+unsigned char *opponent_color;
+double *shooting_frequency;
+
+
+void chose_mode() {
+
+}
+
+void select_opponent_color() {  // After player selects colorblind mode
+
+}
+
+void set_shooting_frequency() {
+
+}
+
+void target_opponent() {
+
+}
+
+void auto_shoot() {
+
+}
+
+int main() {
+    printf("hi");
+
+    return 0;
+}
