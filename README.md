e70b64df9e5f1fe4aec7d0aac3ce334ff61f8ddf
Flora Afroza <afzflora@gmail.com>
2023-04-08 22:10:30 -0400

    new compiling instructions

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 
-The tool that will make your opponents hate you. It's an aimbot designed as a color-detection tool to help target opponents in first person style shooter games. 
-
-Users can set instructions via command-line 
+The tool that will make your opponents hate you. It's an aimbot designed as a color-detection tool to help target opponents in first person style shooter games.Users can set instructions via command-line.
 
 ![](compiling_instructions.png)
 
--- aimbot/aimbot.c
+++ aimbot/aimbot.c
@@ -14,7 +14,7 @@ By Flora Afroza
 #include <curses.h>
 
 
-int *is_running, *auto_mode, *shooting_frequency;
+int *is_running, *auto_shooting_frequency;
 unsigned char *opponent_color;
 
 
@@ -27,53 +27,33 @@ unsigned char *opponent_color;
     }
 
     void set_shooting_frequency(double shots_per_minute) {
-        *shooting_frequency = shots_per_minute;
+        *auto_shooting_frequency = shots_per_minute;
     }
 
     void target_opponent() {       // cursor functions here
 
     }
 
-    void enable_auto_shoot() {      // May enable auto-shooting at the begining of runtime
-        *auto_mode = 1;
-    }
-
     void toggle_state(int *state) {
         *state = abs(*state - 1);
     }
 
-/*
-    void toggle_auto_shooting() {       // Toggled via hot-key shortcut
-        *auto_mode = abs(*auto_mode - 1);
-    }
-
-    void toggle_aimbot() {      // Toggled via hot-key shortcut
-        *is_running = abs(*is_running - 1);
-    }
-*/
 
 int main(int argc, char *argv[]) {
 
-    auto_mode = malloc(sizeof(int));
-    *auto_mode = 0;
     is_running = malloc(sizeof(int));
+    *is_running = 1;
 
-    shooting_frequency = malloc(sizeof(int));
-    *shooting_frequency = 50;   // default
+    auto_shooting_frequency = malloc(sizeof(int));
+    *auto_shooting_frequency = 0;   // 0 means manual mode
 
     if(argc > 0) {
         set_shooting_frequency(atoi(argv[0]));
-        if(argc > 1) {
-            if(argv[1] == "auto") {
-                enable_auto_shoot();
-            }
-        }
     }
     
 
     free(is_running);
-    free(auto_mode);
-    free(shooting_frequency);
+    free(auto_shooting_frequency);
 
     return 0;
 }
